026ee7a81e19af067357ff3dd1aa49a096158e14
NameD NameD <NameD NameD>
2017-02-05 15:29:20 +0100

    Initial commit

---
+++ android/src/pl/firstgamepackage/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.firstgamepackage;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.firstgamepackage.FirstGameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstGameClass(), config);
+	}
+}
---
+++ desktop/src/pl/firstgamepackage/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package pl.firstgamepackage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.firstgamepackage.FirstGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirstGameClass(), config);
+		//1st
+	}
+}
